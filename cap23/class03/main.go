fc7c4ae94ea40c423e1e05a606f3a6821f2f5a64
Pedro Araújo <hpedroaraujo@proton.me>
2025-03-24 20:30:08 -0300

    log package, recover and customized errors.

--- cap23/class03/main.go
+++ cap23/class03/main.go
@@ -1,5 +1,67 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
 
+	fmt.Println("------Exemplo 01------")
+	_, err := os.Open("no-file.txt")
+	if err != nil {
+		// println works as usual, writing the error to stdout
+		fmt.Println("err happened", err)
+	}
+
+	fmt.Println("------Exemplo 02------")
+	if err != nil {
+		// timestamp + error. outputs to std logger
+		log.Println("err happened", err)
+	}
+
+	fmt.Println("------Exemplo 03------")
+	f, err := os.Create("log.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	// setting the log output to a file
+	log.SetOutput(f)
+
+	_, err = os.Open("no-file.txt")
+	if err != nil {
+		log.Println("err happened", err)
+	}
+
+	fmt.Println("check the log.txt fule in the directory.")
+
+	fmt.Println("------Exemplo 04------")
+	defer foo()
+	if err != nil {
+		// println + panic -> panic stop the goroutine but allow defered functions to run
+		// also, panic allow recover()
+		log.Panicln(err)
+	}
+
+	fmt.Println("------Exemplo 05------")
+	bar(err)
+	recover() // see how to use it
+	fmt.Println("the program continues after panic->recover...")
+
+	fmt.Println("------Exemplo 06------")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func foo() {
+	fmt.Println("When os.Exit() is called, deffered functions don't run")
+}
+
+func bar(err error) {
+	if err != nil {
+		log.Panicln(err)
+	}
 }
--- cap23/class05/main.go
+++ cap23/class05/main.go
@@ -1,5 +1,89 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type TypeErrNorgateMath struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (e TypeErrNorgateMath) Error() string {
+	return fmt.Sprintf("a norgate math error occured: %v %v %v", e.lat, e.long, e.err)
+}
+
+var ErrNorgateMath = errors.New("norgate math: square root of negative number")
+
 func main() {
 
+	fmt.Println("------Exemplo01------")
+	_, err := sqrt(-10)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("------Exemplo02------")
+	_, err = sqrtWithVarError(-10)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("------Exemplo03------")
+	_, err = sqrtWithFormatError(-87.55)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("------Exemplo04------")
+	_, err = sqrtWithVarFormatError(-95.40)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("------Exemplo04------")
+	_, err = sqrtWithTypeError(-88.66)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		return 0, errors.New("norgate math: square root of negative number")
+	}
+	return 42, nil
+}
+
+func sqrtWithVarError(f float64) (float64, error) {
+	if f < 0 {
+		return 0, ErrNorgateMath
+	}
+	return 42, nil
+}
+
+func sqrtWithFormatError(f float64) (float64, error) {
+	if f < 0 {
+		return 0, fmt.Errorf("norgate math: square root of negative number: %v", f)
+	}
+	return 42, nil
+}
+
+func sqrtWithVarFormatError(f float64) (float64, error) {
+	if f < 0 {
+		NewErrNorgateMath := fmt.Errorf("norgate math: square root of negative number: %v", f)
+		return 0, NewErrNorgateMath
+	}
+	return 42, nil
+}
+
+func sqrtWithTypeError(f float64) (float64, error) {
+	if f < 0 {
+		NewErrNorgateMath := fmt.Errorf("norgate math: square root of negative number: %v", f)
+		return 0, TypeErrNorgateMath{lat: "90.22N", long: "87.54W", err: NewErrNorgateMath}
+	}
+	return 42, nil
 }
