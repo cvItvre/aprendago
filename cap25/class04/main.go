ed654a0bf574308e1904fd6074d7bdd022628dc5
Pedro Araújo <hpedroaraujo@proton.me>
2025-03-25 14:06:24 -0300

    test put a package to godoc.

---
+++ cap25/class04/main.go
@@ -0,0 +1,9 @@
+// Testing package that will be deleted
+package unnamed
+
+import "fmt"
+
+// This funciont just present the package
+func Foo() {
+	fmt.Println("hello, it's the unnamed package...")
+}
